93493f500899923efa489b5f57d9e7dfa722d4f0
Pavel Chechelev <pashachechelev@gmail.com>
2022-03-25 02:17:17 +0300

    feat: add header component

--- coincap/src/components/app/App.js
+++ coincap/src/components/app/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-
 import './App.scss';
+
+import { Header } from '../header/header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header></Header>
       </div>
     );
   }
---
+++ coincap/src/components/header/header.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './header.scss';
+import logo from './logo.svg';
+
+export class Header extends Component {
+  render() {
+    return (
+      <header className="header">
+        <div className="container">
+          <div className="container__inner">
+            <div className="logo">
+              <img src={logo} alt="Coincap"></img>
+            </div>
+            <div className="top-coins">
+
+              <div className="top-coins__item">
+                <div className="top-coins__item-name">Bitcoin</div>
+                <div className="top-coins__item-price">43897$</div>
+              </div>
+
+              <div className="top-coins__item">
+                <div className="top-coins__item-name">Ethereum</div>
+                <div className="top-coins__item-price">3106$</div>
+              </div>
+
+              <div className="top-coins__item">
+                <div className="top-coins__item-name">Tether</div>
+                <div className="top-coins__item-price">1$</div>
+              </div>
+            </div>
+
+            <div className="user-wallet-info">
+              <div className="user-wallet__current-cost">134,32 USD</div>
+              <div className="user-wallet__different-cost">+2,38</div>
+              <div className="user-wallet__different-cost-percent">(+1,80%)</div>
+              <i class="fa-solid fa-briefcase"></i>
+            </div>
+
+          </div>
+        </div>
+      </header>
+    )
+  }
+} 
