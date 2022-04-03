fddf5a605877b4578ce6f10b3fcd0aa9f8d4d044
Pavel Chechelev <pashachechelev@gmail.com>
2022-04-03 19:56:57 +0300

    feat: add footer

--- coincap/src/components/app/App.js
+++ coincap/src/components/app/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import { Header } from '../header/header';
 import { CryptoTable } from '../crypto-table/crypto-table';
 import { CoinDetails } from '../crypto-item-details/crypto-details';
+import { Footer } from '../footer/footer'
 class App extends Component {
 
   state = {
@@ -24,6 +25,7 @@ class App extends Component {
           <Route path="/" element={<CryptoTable onItemSelected={this.onCoinSelected} />} />
           <Route path="/details" element={<CoinDetails coinId={this.state.selectedCoin} />} />
         </Routes>
+        <Footer></Footer>
       </div>
     );
   }
---
+++ coincap/src/components/footer/footer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './footer.scss';
+export class Footer extends Component {
+
+  render() {
+    return (
+      <footer className="footer">
+        <div className="container">
+          <div className="creator-block">
+            <div className="creator-block__name">Pavel Chechelev</div>
+            <div className="creator-block__github">
+              <a className="github-link" href="https://github.com/Chechelev">GitHub</a>
+            </div>
+          </div>
+        </div>
+      </footer>
+    )
+  };
+};
