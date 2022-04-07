244ac21adacf18389be9023771911cfa13679607
Pavel Chechelev <pashachechelev@gmail.com>
2022-04-07 20:04:11 +0300

    fix: header percent counting

--- coincap/src/components/header/header.js
+++ coincap/src/components/header/header.js
@@ -116,11 +116,13 @@ export class Header extends Component {
     }
 
     const items = this.renderItems(headerCoinList);
-    let currentCost = headerCoinCost
+    let currentCost = headerCoinCost;
     const localeCost = this.getLocaleCost();
     const difference = parseFloat(currentCost - localeCost).toFixed(2);
     const diff = difference > 0 ? '+' + (difference) : (difference);
-    let percent = parseFloat(((currentCost - localeCost) * 100) / localeCost).toFixed(2);
+    let percent = parseFloat(((diff) * 100) / currentCost).toFixed(2);
+    let totalPercent = (percent < 0 ? + percent : "" + percent);
+
 
 
     return (
@@ -140,7 +142,7 @@ export class Header extends Component {
               <div className="user-wallet-info" onClick={this.showModal}>
                 <div className="user-wallet__current-cost">{parseFloat(currentCost).toFixed(2)} $</div>
                 <div className="user-wallet__different-cost">{`${diff == NaN ? 0 : diff}`}</div>
-                <div className="user-wallet__different-cost-percent">{percent == NaN ? 0 : percent}%</div>
+                <div className="user-wallet__different-cost-percent">{(isNaN(percent) || percent == Infinity) ? 0 : totalPercent}%</div>
                 <i className="fa-solid fa-briefcase"></i>
               </div>
 
