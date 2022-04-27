884d0457f1008913e613341e6bfbf220a23c301f
Pavel Chechelev <pashachechelev@gmail.com>
2022-04-27 22:33:30 +0300

    feat: add more header-tests

--- coincap/src/components/layout/header/header-wallet/__test__/header-wallet.test.js
+++ coincap/src/components/layout/header/header-wallet/__test__/header-wallet.test.js
@@ -2,18 +2,52 @@ import React from "react";
 import { render, screen, fireEvent, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import '@testing-library/react';
+require('jest-localstorage-mock');
 import { HeaderWallet } from '../header-wallet';
 
 afterEach(() => {
-  cleanup();
+  cleanup()
 })
 
-it('renders one element', () => {
-  jest.spyOn(window.localStorage.__proto__, 'setItem');
-  window.localStorage.__proto__.setItem = jest.fn();
 
-  // assertions as usual:
-  expect(localStorage.setItem).toHaveBeenCalled(1);
+it('renders element', () => {
+  const headerCoinCost = 544123.123543;
 
+  render(<HeaderWallet headerCoinCost={headerCoinCost} onClick={true ? true : false} ></HeaderWallet>);
+
+  const userWalletElement = screen.getByTestId("user-wallet-info-test");
+  expect(userWalletElement).toBeInTheDocument();
 });
 
+it('checks the current cost element', () => {
+  const headerCoinCost = 544123.123543;
+
+  render(<HeaderWallet headerCoinCost={headerCoinCost} onClick={true ? true : false} ></HeaderWallet>);
+  const userWalletElement = screen.getByTestId("user-wallet__current-cost-test");
+
+  const userWalletDifferenceElement = screen.getByTestId("user-wallet__different-cost-test");
+  expect(userWalletDifferenceElement).toBeInTheDocument();
+
+  expect(userWalletElement).toBeInTheDocument();
+  expect(userWalletElement).toHaveTextContent(544123.12);
+});
+
+
+it('checks the locale cost element', () => {
+  const headerCoinCost = 544123.123543;
+
+  const KEY = "walletData",
+    VALUE = '[{"id":"bitcoin","name":"Bitcoin","price":"38953.6908005677844877","amount":"1"}]';
+
+  render(<HeaderWallet headerCoinCost={headerCoinCost} onClick={true ? true : false} ></HeaderWallet>);
+
+  const userWalletDifferenceElement = screen.getByTestId("user-wallet__different-cost-test");
+  expect(userWalletDifferenceElement).toBeInTheDocument();
+
+  dispatch(action.update(KEY, VALUE));
+  expect(localStorage.setItem).toHaveBeenLastCalledWith(1);
+  expect(localStorage.__STORE__[KEY]).toBe(VALUE);
+  expect(Object.keys(localStorage.__STORE__).length).toBe(1);
+
+  expect(userWalletDifferenceElement).toHaveTextContent(544123.12);
+});
--- coincap/src/components/layout/header/header-wallet/header-wallet.js
+++ coincap/src/components/layout/header/header-wallet/header-wallet.js
@@ -11,9 +11,9 @@ export function HeaderWallet({ headerCoinCost, showModal }) {
   let totalPercent = (percent < 0 ? + percent : "" + percent);
 
   return (
-    <div className="user-wallet-info" onClick={showModal}>
-      <div className="user-wallet__current-cost">{parseFloat(currentCost).toFixed(2)} $</div>
-      <div className="user-wallet__different-cost">{`${diff == NaN ? 0 : diff}`}</div>
+    <div data-testId="user-wallet-info-test" className="user-wallet-info" onClick={showModal}>
+      <div data-testid="user-wallet__current-cost-test" className="user-wallet__current-cost">{parseFloat(currentCost).toFixed(2)} $</div>
+      <div data-testid="user-wallet__different-cost-test" className="user-wallet__different-cost">{`${diff == NaN ? 0 : diff}`}</div>
       <div className="user-wallet__different-cost-percent">{(isNaN(percent) || percent == Infinity || percent == -Infinity) ? 0 : totalPercent}%</div>
       <i className="fa-solid fa-briefcase"></i>
     </div>
