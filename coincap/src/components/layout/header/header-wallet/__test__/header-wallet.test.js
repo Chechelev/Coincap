e385b5e2baf9f41c9d9667887c4223e110d2dfd1
Pavel Chechelev <pashachechelev@gmail.com>
2022-04-29 17:24:22 +0300

    feat: add full header tests

--- coincap/src/components/layout/header/header-wallet/__test__/header-wallet.test.js
+++ coincap/src/components/layout/header/header-wallet/__test__/header-wallet.test.js
@@ -16,6 +16,7 @@ it('renders element', () => {
 
   const userWalletElement = screen.getByTestId("user-wallet-info-test");
   expect(userWalletElement).toBeInTheDocument();
+
 });
 
 it('checks the current cost element', () => {
@@ -23,12 +24,11 @@ it('checks the current cost element', () => {
 
   render(<HeaderWallet headerCoinCost={headerCoinCost} ></HeaderWallet>);
 
-  const userWalletElement = screen.getByTestId("user-wallet__current-cost-test");
-  const userWalletDifferenceElement = screen.getByTestId("user-wallet__different-cost-test");
+  const userWalletCurrentCostElement = screen.getByTestId("user-wallet__current-cost-test");
 
-  expect(userWalletDifferenceElement).toBeInTheDocument();
-  expect(userWalletElement).toBeInTheDocument();
-  expect(userWalletElement).toHaveTextContent(544123.12);
+  expect(userWalletCurrentCostElement).toBeInTheDocument();
+  expect(userWalletCurrentCostElement).toHaveTextContent(544123.12);
+
 });
 
 it('checks the diffrence cost element', () => {
@@ -46,15 +46,81 @@ it('checks the diffrence cost element', () => {
 
   render(<HeaderWallet headerCoinCost={headerCoinCost} ></HeaderWallet>);
 
-  const userWalletCurrentCostElement = screen.getByTestId("user-wallet__current-cost-test");
   const userWalletDifferenceElement = screen.getByTestId("user-wallet__different-cost-test");
+
+  expect(userWalletDifferenceElement).toBeInTheDocument();
+  expect(localStorage.getItem('walletData')).toBe("[{\"id\":\"bitcoin\",\"name\":\"Bitcoin\",\"price\":\"39144.0257023189199431\",\"amount\":\"1\"}]");
+  expect(userWalletDifferenceElement).toHaveTextContent('+504979.1');
+
+});
+
+
+it('checks if the difference < 0', () => {
+  const headerCoinCost = 54.123543;
+  const walletData = [
+    {
+      id: "bitcoin",
+      name: "Bitcoin",
+      price: "39144.0257023189199431",
+      amount: "1"
+    }
+  ];
+
+  localStorage.setItem('walletData', JSON.stringify(walletData));
+
+  render(<HeaderWallet headerCoinCost={headerCoinCost} ></HeaderWallet>);
+
+  const userWalletDifferenceElement = screen.getByTestId("user-wallet__different-cost-test");
+
+  expect(userWalletDifferenceElement).toBeInTheDocument();
+  expect(localStorage.getItem('walletData')).toBe("[{\"id\":\"bitcoin\",\"name\":\"Bitcoin\",\"price\":\"39144.0257023189199431\",\"amount\":\"1\"}]");
+  expect(userWalletDifferenceElement).toHaveTextContent('-39089.90');
+
+});
+
+it('checks the percent', () => {
+  const headerCoinCost = 54412.123543;
+  const walletData = [
+    {
+      id: "bitcoin",
+      name: "Bitcoin",
+      price: "39144.0257023189199431",
+      amount: "1"
+    }
+  ];
+
+  localStorage.setItem('walletData', JSON.stringify(walletData));
+
+  render(<HeaderWallet headerCoinCost={headerCoinCost} ></HeaderWallet>);
+
   const userWalletPercentElement = screen.getByTestId("user-wallet__different-cost-percent");
 
-  expect(userWalletCurrentCostElement).toBeInTheDocument();
-  expect(userWalletDifferenceElement).toBeInTheDocument();
   expect(userWalletPercentElement).toBeInTheDocument();
-
   expect(localStorage.getItem('walletData')).toBe("[{\"id\":\"bitcoin\",\"name\":\"Bitcoin\",\"price\":\"39144.0257023189199431\",\"amount\":\"1\"}]");
+  expect(userWalletPercentElement).toHaveTextContent('28.06%');
+
+});
+
+it('checks the percent', () => {
+  const headerCoinCost = 30412.123543;
+  const walletData = [
+    {
+      id: "bitcoin",
+      name: "Bitcoin",
+      price: "39144.0257023189199431",
+      amount: "1"
+    }
+  ];
+
+  localStorage.setItem('walletData', JSON.stringify(walletData));
+
+  render(<HeaderWallet headerCoinCost={headerCoinCost} ></HeaderWallet>);
+
+  const userWalletPercentElement = screen.getByTestId("user-wallet__different-cost-percent");
+
+  expect(userWalletPercentElement).toBeInTheDocument();
+  expect(localStorage.getItem('walletData')).toBe("[{\"id\":\"bitcoin\",\"name\":\"Bitcoin\",\"price\":\"39144.0257023189199431\",\"amount\":\"1\"}]");
+  expect(userWalletPercentElement).toHaveTextContent('-28.71%');
 
 });
 
@@ -66,6 +132,7 @@ it('calls onClick prop when clicked', () => {
 
   fireEvent.click(userWalletElement);
   expect(handleClick).toHaveBeenCalledTimes(1);
+
 });
 
 
