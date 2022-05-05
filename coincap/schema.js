e1df450d538c77866ce9a983df7bb107b15d0451
Pavel Chechelev <pashachechelev@gmail.com>
2022-05-06 01:48:43 +0300

    feat: add schema and server files

---
+++ coincap/schema.js
@@ -0,0 +1,68 @@
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLSchema,
+  GraphQLID } = require('graphql');
+
+const axios = require('axios');
+
+const coinTypes = new GraphQLObjectType({
+  name: 'Coins',
+  fields: () => ({
+    data: { type: new GraphQLList(dataType) }
+  })
+});
+
+const coinId = new GraphQLObjectType({
+  name: 'CoinId',
+  fields: () => ({
+    data: { type: (dataType) }
+  })
+});
+
+const dataType = new GraphQLObjectType({
+  name: 'Coin',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    priceUsd: { type: GraphQLString },
+    supply: { type: GraphQLString },
+    marketCapUsd: { type: GraphQLString },
+    volumeUsd24Hr: { type: GraphQLString },
+    changePercent24Hr: { type: GraphQLString },
+    vwap24Hr: { type: GraphQLString },
+  })
+});
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    coins: {
+      type: (coinTypes),
+      resolve(parents, args) {
+        return axios
+          .get('https://api.coincap.io/v2/assets')
+          .then(res => res.data);
+      }
+    },
+
+    coin: {
+      type: coinId,
+      args: {
+        id: { type: GraphQLString },
+      },
+      resolve(parent, args) {
+        return axios
+          .get(`https://api.coincap.io/v2/assets/${args.id}`)
+          .then(res => res.data);
+      }
+    },
+  }
+});
+
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
--- coincap/src/index.js
+++ coincap/src/index.js
@@ -3,17 +3,29 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import configureStore from './store/configureStore';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
 
 import './index.scss';
 import App from './components/app/App';
 
 const store = configureStore();
 
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql',
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </ApolloProvider>,
   document.getElementById('root')
 );
